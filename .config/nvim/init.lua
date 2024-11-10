8d8febae0feacd9334d8c284b3ff69c3dc2803bf
xshlc <heechuw@gmail.com>
2024-11-09 22:13:59 -0500

    neovim lsp and autocompletion setup

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -27,5 +27,7 @@ require('lazy').setup({
     require 'plugins.bufferline',
     require 'plugins.lualine',
     require 'plugins.treesitter',
-    require 'plugins.telescope'
+    require 'plugins.telescope',
+    require 'plugins.lsp',
+    require 'plugins.autocompletion'
 })
--- .config/nvim/lua/core/keymaps.lua
+++ .config/nvim/lua/core/keymaps.lua
@@ -58,7 +58,7 @@ vim.keymap.set('n', '<Right>', ':vertical resize +2<CR>', opts)
 vim.keymap.set('n', '<Tab>', ':bnext<CR>', opts)
 vim.keymap.set('n', '<S-Tab>', ':bprevious<CR>', opts)
 -- close a buffer with leader+x
-vim.keymap.set('n', '<leader>x', ':Bdelete!<CR>', opts) -- close buffer
+vim.keymap.set('n', '<leader>x', ':bdelete!<CR>', opts) -- close buffer
 -- new buffer with leader+b
 vim.keymap.set('n', '<leader>b', '<cmd> enew <CR>', opts) -- new buffer
 

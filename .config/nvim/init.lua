1132e6ba02eb0fcbd7d3c54af3d5071d615c736a
xshlc <heechuw@gmail.com>
2024-11-09 23:47:25 -0500

    neovim greeter alpha plugin

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -6,11 +6,11 @@ require 'core.keymaps'
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-    local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
-    local out = vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
-    if vim.v.shell_error ~= 0 then
-        error('Error cloning lazy.nvim:\n' .. out)
-    end
+  local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
+  local out = vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
+  if vim.v.shell_error ~= 0 then
+    error('Error cloning lazy.nvim:\n' .. out)
+  end
 end ---@diagnostic disable-next-line: undefined-field
 vim.opt.rtp:prepend(lazypath)
 
@@ -22,14 +22,15 @@ vim.opt.rtp:prepend(lazypath)
 --    :Lazy update
 -- NOTE: Here is where you install your plugins.
 require('lazy').setup {
-    require 'plugins.neotree',
-    require 'plugins.colortheme',
-    require 'plugins.bufferline',
-    require 'plugins.lualine',
-    require 'plugins.treesitter',
-    require 'plugins.telescope',
-    require 'plugins.lsp',
-    require 'plugins.autocompletion',
-    require 'plugins.none-ls',
-    require 'plugins.gitsigns',
+  require 'plugins.neotree',
+  require 'plugins.colortheme',
+  require 'plugins.bufferline',
+  require 'plugins.lualine',
+  require 'plugins.treesitter',
+  require 'plugins.telescope',
+  require 'plugins.lsp',
+  require 'plugins.autocompletion',
+  require 'plugins.none-ls',
+  require 'plugins.gitsigns',
+  require 'plugins.alpha',
 }

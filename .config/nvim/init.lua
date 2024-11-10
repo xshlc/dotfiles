a2d1d4351056f6678e91b4bcd7638307aa0b127d
heechuw@gmail.com <heechuw@gmail.com>
2024-11-09 23:32:18 -0500

    neovim autoformatter none-ls & formatted all lua files

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -6,11 +6,11 @@ require 'core.keymaps'
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
-  local out = vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
-  if vim.v.shell_error ~= 0 then
-    error('Error cloning lazy.nvim:\n' .. out)
-  end
+    local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
+    local out = vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
+    if vim.v.shell_error ~= 0 then
+        error('Error cloning lazy.nvim:\n' .. out)
+    end
 end ---@diagnostic disable-next-line: undefined-field
 vim.opt.rtp:prepend(lazypath)
 
@@ -21,7 +21,7 @@ vim.opt.rtp:prepend(lazypath)
 --  To update plugins you can run
 --    :Lazy update
 -- NOTE: Here is where you install your plugins.
-require('lazy').setup({
+require('lazy').setup {
     require 'plugins.neotree',
     require 'plugins.colortheme',
     require 'plugins.bufferline',
@@ -29,5 +29,6 @@ require('lazy').setup({
     require 'plugins.treesitter',
     require 'plugins.telescope',
     require 'plugins.lsp',
-    require 'plugins.autocompletion'
-})
+    require 'plugins.autocompletion',
+    require 'plugins.none-ls',
+}
--- .config/nvim/lua/plugins/colortheme.lua
+++ .config/nvim/lua/plugins/colortheme.lua
@@ -4,24 +4,23 @@ return {
     lazy = false,
     priority = 1000,
     config = function()
-      vim.g.gruvbox_baby_background_color = "dark" -- dark or medium
-      vim.g.gruvbox_baby_transparent_mode = false
-      vim.g.gruvbox_baby_comment_style = "bold"
+        vim.g.gruvbox_baby_background_color = 'dark' -- dark or medium
+        vim.g.gruvbox_baby_transparent_mode = false
+        vim.g.gruvbox_baby_comment_style = 'bold'
 
-      -- vim.g.gruvbox_baby_keyword_style = "italic"
-    
-      -- Load the colorscheme
-      vim.cmd [[colorscheme gruvbox-baby]]
+        -- vim.g.gruvbox_baby_keyword_style = "italic"
 
-      -- Toggle background transparency
-      local toggle_transparency = function()
-          bg_transparent = not bg_transparent
-          vim.g.gruvbox_baby_transparent_mode = bg_transparent
-          vim.cmd [[colorscheme gruvbox-baby]]
-      end
+        -- Load the colorscheme
+        vim.cmd [[colorscheme gruvbox-baby]]
 
-      -- Map a key to the toggle background transparency
-      vim.keymap.set('n','<leader>bg', toggle_transparency, {noremap=true, silent=true})
-    end
- }
+        -- Toggle background transparency
+        local toggle_transparency = function()
+            bg_transparent = not bg_transparent
+            vim.g.gruvbox_baby_transparent_mode = bg_transparent
+            vim.cmd [[colorscheme gruvbox-baby]]
+        end
 
+        -- Map a key to the toggle background transparency
+        vim.keymap.set('n', '<leader>bg', toggle_transparency, { noremap = true, silent = true })
+    end,
+}
